2c45144a14164baf61237d81130eaa2d2bbb2021
Thomas Gehrmann <s6thgehr@gmail.com>
2024-10-19 21:13:38 +0200

    Update bytemuck dependency

--- programs/eki/src/state/exits.rs
+++ programs/eki/src/state/exits.rs
@@ -1,23 +1,21 @@
 use anchor_lang::prelude::*;
 
-#[account(zero_copy(unsafe))]
+#[account(zero_copy)]
 #[derive(InitSpace)]
 pub struct Exits {
     pub token_a: [u64; 262144],
     pub token_b: [u64; 262144],
-    pub pointer: u32,
-    pub bump: u8,
+    pub pointer: u64,
 }
 
 impl Exits {
     pub const SEED_PREFIX: &'static str = "exits";
 
-    pub fn new(bump: u8) -> Self {
+    pub fn new() -> Self {
         Self {
             token_a: [0; 262144],
             token_b: [0; 262144],
             pointer: 0,
-            bump,
         }
     }
 }
